59424e12d3ca19d8db368e8bec5516fa621e5567
swappel <sebastian.lauber38@gmail.com>
2025-12-09 00:05:20 +0100

    Initial commit

---
+++ include/Server.h
@@ -0,0 +1,15 @@
+//
+// Created by elanda on 12/8/25.
+//
+
+#ifndef AFOP_TRANSLATOR_BACKEND_SERVER_H
+#define AFOP_TRANSLATOR_BACKEND_SERVER_H
+
+namespace network
+{
+    class Server
+    {
+    };
+} // network
+
+#endif //AFOP_TRANSLATOR_BACKEND_SERVER_H
---
+++ src/network/Server.cpp
@@ -0,0 +1,9 @@
+//
+// Created by elanda on 12/8/25.
+//
+
+#include "Server.h"
+
+namespace network
+{
+} // network
